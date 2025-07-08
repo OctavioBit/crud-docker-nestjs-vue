76a7720af145b8b2fcd7a271c1120de3d78b6a83
OctavioBit <gera.fuentes.exactas@gmail.com>
2025-07-08 00:57:37 -0300

    estetica

--- agileBoard.md
+++ agileBoard.md
@@ -1,12 +1,12 @@
 PENDIENTE
 * Tests automaticos, puede ser con Jest o Vite o eso que tiene vuetify 3 VITEST
 * Debugger -> https://blog.itaywol.com/dockerizing-nestjs-application-and-debugging
-* Pulido final
 
 EN CURSO
-* Tema Claro, estetica
+* Pulido final
 
 TERMINADOS
+* Tema Claro, estetica
 * ver de que lleguen a los actions DTOs, para evitar parsear
 * Las variables del .env no las toma el archivo schema.prisma
 * birthdate por rango
--- frontend/src/pages/about.vue
+++ frontend/src/pages/about.vue
@@ -1,30 +1,23 @@
 <template>
-  <div class="about">
-    <h1>Acerca de ...</h1>
-
-    <v-btn @click="saludar">
-      {{ pepe }}
-    </v-btn>    
+  <div class="d-flex justify-center mb-4">
+    <h1>My Dog House</h1>
+  </div>
+  <div class="d-flex justify-center flex-wrap gap-2 mb-6">
+    <v-chip class="mr-3 ml-3" color="green">NodeJS</v-chip>
+    <v-chip class="mr-3 ml-3" color="green">NestJS</v-chip>
+    <v-chip class="mr-3 ml-3" color="green">CQRS</v-chip>
+    <v-chip class="mr-3 ml-3" color="orange">MySQL</v-chip>
+    <v-chip class="mr-3 ml-3" color="red">Vue</v-chip>
+    <v-chip class="mr-3 ml-3" color="red">Vite</v-chip>
+    <v-chip class="mr-3 ml-3" color="red">Vuetify</v-chip>
+    <v-chip class="mr-3 ml-3" color="red">Pinia</v-chip>
+    <v-chip class="mr-3 ml-3" color="red">Axios</v-chip>
+    <v-chip class="mr-3 ml-3" color="blue">Docker</v-chip>
+  </div>
+  <div class="d-flex justify-center mb-4">
+    <h2>GitHub</h2>
+  </div>
+  <div class="d-flex justify-center mb-4">
+    <a href="https://github.com/OctavioBit/backendnodecqrs">https://github.com/OctavioBit/backendnodecqrs</a>
   </div>
 </template>
-
-<script>
-
-export default {
-  data: () => ({
-    pepe: 16
-  }),
-  methods: {
-    saludar: function(){
-       console.log('Hola');  
-       this.contar();
-      },
-    contar: function(){
-        this.pepe++;
-    }
-  },
-  mounted: function(){
-    this.pepe = 0;
-  }
-}
-</script>
